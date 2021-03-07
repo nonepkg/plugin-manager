f7acda28a2752cca61622ecaf942e0a295cf4df5
陈末 <50312681+Jigsaw111@users.noreply.github.com>
2021-03-06 22:31:23 -0800

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# TRPGLogger
+
+*基于 [nonebot2](https://github.com/nonebot/nonebot2) 以及 [go-cqhttp](https://github.com/Mrs4s/go-cqhttp) 插件管理器*
+
+[![License](https://img.shields.io/github/license/Jigsaw111/nonebot_plugin_manager)](LICENSE)
+![Python Version](https://img.shields.io/badge/python-3.7+-blue.svg)
+![NoneBot Version](https://img.shields.io/badge/nonebot-2+-red.svg)
+![Pypi Version](https://img.shields.io/pypi/v/nonebot-plugin-manager.svg)
+
+### 安装
+
+* 使用nb-cli（推荐）  
+
+```bash
+nb plugin install nonebot_plugin_manager
+```
+
+* 使用poetry
+
+```bash
+poetry add nonebot_plugin_manager
+```
+
+### 开始使用
+
+`.plugin list` 查看插件列表
+`.plugin block 插件名` 屏蔽插件
+`.plugin unblock 插件名` 启用插件
+
+### TO DO
+
+[ ] 分群插件管理
---
+++ nonebot_plugin_manager/__init__.py
@@ -0,0 +1,78 @@
+import os
+
+from nonebot.plugin import get_loaded_plugins, Matcher
+from nonebot import on_command
+from nonebot.message import run_preprocessor
+from nonebot.adapters.cqhttp import Event, Bot
+from nonebot.typing import T_State
+from nonebot.exception import IgnoredException
+
+DATA_PATH = "data/manager/block_list"
+
+plugin_list = on_command('plugin')
+@plugin_list.handle()
+async def _(bot: Bot, event: Event):
+    block_list = get_block_list()
+    plugin_list = get_plugin_list()
+    message = str(event.get_message()).strip()
+    if message == 'list':
+        result='插件列表：'
+        for plugin in plugin_list:
+            result+='\n'
+            if plugin in block_list:
+                result+='× '
+            else:
+                result+='√ '
+            result+=plugin
+        await bot.send(event,result)
+    elif message[:5] == 'block':
+        plugin = message[6:].strip()
+        if plugin in plugin_list and plugin != 'nonebot_plugin_manager':
+            if plugin in block_list:
+                await bot.send(event, "该插件已屏蔽！")
+            else:
+                block_list.append(plugin)
+                set_block_list(block_list)
+        else:
+            await bot.send(event, "该插件不存在！")
+    elif message[:7] == 'unblock':
+        plugin = message[8:].strip()
+        if plugin in plugin_list:
+            if not plugin in block_list:
+                await bot.send(event, "该插件未被屏蔽！")
+            else:
+                block_list.remove(plugin)
+                set_block_list(block_list)
+        else:
+            await bot.send(event, "该插件不存在！")
+
+@run_preprocessor
+async def _(matcher: Matcher, bot: Bot, event: Event, state: T_State):
+    block_list = get_block_list()
+    if matcher.module in block_list:
+        raise IgnoredException("reason")
+
+def get_block_list() -> list:
+    if not os.path.exists(DATA_PATH):
+        f = open(DATA_PATH, 'w')
+        f.close()
+    block_list = []
+    f = open(DATA_PATH,'r')
+    plugin_list=f.read()
+    f.close()
+    plugin_list=plugin_list.split('\n')
+    for plugin in plugin_list:
+        if plugin != '':
+            block_list.append(plugin)
+    return block_list
+
+def get_plugin_list()->list:
+    plugin_list = []
+    for plugin in get_loaded_plugins():
+        plugin_list.append(plugin.name)
+    return plugin_list  
+
+def set_block_list(block_list):
+    f = open(DATA_PATH, 'w')
+    f.writelines(plugin+'\n' for plugin in block_list)
+    f.close()
