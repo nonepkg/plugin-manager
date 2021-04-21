28fb1f4ad77b3dbbc7993fa098ea3109ad6eec81
Jigsaw <j1g5aw@foxmail.com>
2021-04-21 20:04:54 +0800

    :recycle: Refactor data.py (now it's OO)

--- nonebot_plugin_manager/data.py
+++ nonebot_plugin_manager/data.py
@@ -1,134 +1,149 @@
 import yaml
 from pathlib import Path
-from typing import Any, Dict, Iterable, Optional
+from typing import Dict, List, Optional, Union
 
-__DATA_PATH = Path() / "data" / "manager" / "plugin_list.yml"
+class PluginList:
+    __path:Path
+    __plugin_list:Dict
 
+    def __init__(self,path:Path = Path() / "data" / "manager" / "plugin_list.yml"):
+        self.__path = path
+        self.__load()
+ 
+    def get_plugin(self,
+        type: Optional[str] = None,
+        user_id: Optional[int] = None,
+        group_id: Optional[int] = None,
+    ) -> Dict[str,bool]:
+        result = {}
+        for plugin in self.__plugin_list:
+            if type == "ignore" or not self.__plugin_list[plugin]["ignore"]:
+                if self.__plugin_list[plugin]["global"]:
+                    if not type == "group" and user_id in self.__plugin_list[plugin]["user"]:
+                        if self.__plugin_list[plugin]["user"][user_id]:
+                            result[plugin]=True
+                        else:
+                            result[plugin]=False
+                    else:
+                        if not type == "user" and group_id in self.__plugin_list[plugin]["group"]:
+                            if self.__plugin_list[plugin]["group"][group_id]:
+                                result[plugin]=True
+                            else:
+                                result[plugin]=False
+                        else:
+                            result[plugin]=True
+                else:
+                    if not type == "user" and group_id in self.__plugin_list[plugin]["group"]:
+                        if self.__plugin_list[plugin]["group"][group_id]:
+                            result[plugin]=True
+                        else:
+                            result[plugin]=False
+                    else:
+                        if not type == "group" and user_id in self.__plugin_list[plugin]["user"]:
+                            if self.__plugin_list[plugin]["user"][user_id]:
+                                result[plugin]=True
+                            else:
+                                result[plugin]=False
+                        else:
+                            result[plugin]=False
+        return result
 
-def get_plugin_list(
-    type: Optional[str] = None,
-    user_id: Optional[int] = None,
-    group_id: Optional[int] = None,
-    show_ignore: bool = False,
-) -> Dict[str, bool]:
+    def update_plugin(self,plugins: Dict[str,bool]) -> "PluginList":
+        other = PluginList().__unignore_plugin(plugins).__ignore_plugin(plugins)
+        self.__unignore_plugin(plugins).__ignore_plugin(other.get_plugin()).__dump()
+        return self
 
-    plugin_list = __load_plugin_list()
-    tmp_plugin_list = {}
-
-    for plugin in plugin_list:
-        if (
-            show_ignore
-            and plugin_list[plugin]["ignore"] is not None
-            or plugin_list[plugin]["ignore"] == False
-        ):
-            if plugin_list[plugin]["global"] is not None:
-                tmp_plugin_list[plugin] = plugin_list[plugin]["global"]
-            elif type == "user" or user_id in plugin_list[plugin]["user"]:
-                tmp_plugin_list[plugin] = plugin_list[plugin]["user"][user_id]
-            elif type == "group" or group_id in plugin_list[plugin]["group"]:
-                tmp_plugin_list[plugin] = plugin_list[plugin]["group"][group_id]
-            elif type == "default" or plugin not in tmp_plugin_list:
-                tmp_plugin_list[plugin] = plugin_list[plugin]["default"]
-
-    return tmp_plugin_list
-
-
-def auto_update_plugin_list(loaded_plugin_list: Dict[str, bool]):
-    plugin_list = __load_plugin_list()
-
-    for plugin in loaded_plugin_list:
-        if plugin not in plugin_list:
-            plugin_list[plugin] = {
-                "global": None,
-                "user": {},
-                "group": {},
-                "default": True,
-            }
-        plugin_list[plugin]["ignore"] = not loaded_plugin_list[plugin]
-
-    for plugin in plugin_list:
-        if plugin not in loaded_plugin_list:
-            plugin_list[plugin]["ignore"] = None
-
-    __dump_plugin_list(plugin_list)
-
-
-def block_plugin(
-    plugins: Iterable[str],
-    type: Optional[str] = None,
-    user_id: Optional[int] = None,
-    group_id: Optional[int] = None,
-) -> Dict[str, Optional[bool]]:
-    return __update_plugin_list(plugins, True, type, user_id, group_id)
-
-
-def unblock_plugin(
-    plugins: Iterable[str],
-    type: Optional[str] = None,
-    user_id: Optional[int] = None,
-    group_id: Optional[int] = None,
-) -> Dict[str, Optional[bool]]:
-    return __update_plugin_list(plugins, False, type, user_id, group_id)
-
-
-# 更新插件列表
-def __update_plugin_list(
-    plugins: Iterable[str],
-    block: bool,
-    type: Optional[str] = None,
-    user_id: Optional[int] = None,
-    group_id: Optional[int] = None,
-) -> Dict[str, Optional[bool]]:
-
-    plugin_list = __load_plugin_list()
-
-    result = {}
-
-    for plugin in plugins:
-        if plugin in plugin_list and plugin_list[plugin]["ignore"] == False:
-            if type == "global" or type == "default":
-                status = plugin_list[plugin][type]
-            elif type == "user" or user_id in plugin_list[plugin]["user"]:
-                status = plugin_list[plugin]["user"][user_id]
-            elif type == "group" or group_id in plugin_list[plugin]["group"]:
-                status = plugin_list[plugin]["group"][group_id]
-            else:
-                status = None
-
-            if type == "global" or not plugin_list[plugin]["global"] == False:
-                status = not block
-                result[plugin] = True
-            else:
-                result[plugin] = False
-
-            if status is not None:
-                if type == "global" or type == "default":
-                    plugin_list[plugin][type] = status
+    # 禁用插件
+    def block_plugin(self,
+        plugins: List[str],
+        type: Optional[str] = None,
+        user_id: Optional[int] = None,
+        group_id: Optional[int] = None,
+    ) -> Dict[str, bool]:
+        result = {}
+        for plugin in plugins:
+            if plugin in self.__plugin_list and not self.__plugin_list[plugin]["ignore"] and self.__plugin_list[plugin]["global"]:
+                result[plugin]=True
+                if type == "global":
+                    self.__plugin_list[plugin]=False
                 elif type == "user" or user_id:
-                    plugin_list[plugin]["user"][user_id] = status
+                    self.__plugin_list[plugin]["user"][user_id] = False
                 elif type == "group" or group_id:
-                    plugin_list[plugin]["group"][group_id] = status
-        else:
-            result[plugin] = None
+                    self.__plugin_list[plugin]["group"][group_id] = False
+            else:
+                result[plugin]=False
+        self.__dump()
+        return result
 
-    __dump_plugin_list(plugin_list)
+    # 解禁插件
+    def unblock_plugin(self,
+        plugins: List[str],
+        type: Optional[str] = None,
+        user_id: Optional[int] = None,
+        group_id: Optional[int] = None,
+    ) -> Dict[str, bool]:
+        result = {}
+        for plugin in plugins:
+            if plugin in self.__plugin_list and not self.__plugin_list[plugin]["ignore"] and (type=="group" or self.__plugin_list[plugin]["global"]):
+                result[plugin]=True
+                if type == "global":
+                    self.__plugin_list[plugin]=False
+                elif type == "user" or user_id:
+                    self.__plugin_list[plugin]["user"][user_id] = False
+                elif type == "group" or group_id:
+                    self.__plugin_list[plugin]["group"][group_id] = False
+            else:
+                result[plugin]=False
+        self.__dump()
+        return result
 
-    return result
+    # 忽略插件
+    def __ignore_plugin(self,plugins:Dict[str,bool]) -> "PluginList":
+        for plugin in plugins:
+            if plugin in self.__plugin_list:
+                self.__plugin_list[plugin]["ignore"]=True
+            else:
+                self.__plugin_list[plugin]={"ignore":plugins[plugin],"global":True,"user":{},"group":{}}
+        return self 
+
+    # 取消忽略插件
+    def __unignore_plugin(self,plugins:Dict[str,bool]) -> "PluginList":
+        for plugin in plugins:
+            if plugin in self.__plugin_list:
+                self.__plugin_list[plugin]["ignore"]=False
+            else:
+                self.__plugin_list[plugin]={"ignore":plugins[plugin],"global":True,"user":{},"group":{}}
+        return self
+
+    # 添加插件（暂时不会用到）
+    def __add_plugin(self, plugins: List[str],) -> "PluginList":
+        for plugin in plugins:
+            if plugin not in self.__plugin_list:
+                self.__plugin_list[plugin]={"ignore":plugins[plugin],"global":True,"user":{},"group":{}}
+        return self
+
+    # 移除插件（暂时不会用到）
+    def __remove_plugin(self, plugins: List[str],) -> "PluginList":
+        for plugin in plugins:
+            if plugin in self.__plugin_list:
+                self.__plugin_list.pop(plugin)
+        return self 
 
 
-# 加载插件列表
-def __load_plugin_list() -> Dict[str, Any]:
-    try:
-        return yaml.safe_load(__DATA_PATH.open("r", encoding="utf-8"))
-    except FileNotFoundError:
-        return {}
+    # 加载插件列表
+    def __load(self) -> Dict[str, Dict[str,Union[bool,Dict[int,bool]]]]:
+        try:
+            self.__plugin_list= yaml.safe_load(self.__path.open("r", encoding="utf-8"))
+        except FileNotFoundError:
+            self.__plugin_list={}
+        return self
 
 
-# 保存插件列表
-def __dump_plugin_list(plugin_list: Dict[str, Any]):
-    __DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
-    yaml.dump(
-        plugin_list,
-        __DATA_PATH.open("w", encoding="utf-8"),
-        allow_unicode=True,
-    )
+    # 保存插件列表
+    def __dump(self):
+        self.__path.parent.mkdir(parents=True, exist_ok=True)
+        yaml.dump(
+            self.__plugin_list,
+            self.__path.open("w", encoding="utf-8"),
+            allow_unicode=True,
+        )
